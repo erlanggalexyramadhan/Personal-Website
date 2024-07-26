e0ba83bd5d034e38820e980314c2d709bf640dae
Erlangga Lexy Ramadhan <erlanggalexyramadhan25@gmail.com>
2024-07-26 15:01:27 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,7 @@
 const nextConfig = {
     output: "export",
     basePath: '/Personal-Website',
-    images: {
-        domains: ['aceternity.com'],
     },
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'firebasestorage.googleapis.com',
-            }
-        ]
-
-    }
-
 };
 
 export default nextConfig;
